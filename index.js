a2ecc064c13985c694e3b88f3e170a76a719941b
Urielstam <urielstamelman@gmail.com>
2023-02-02 22:39:49 +0200

    Refactor code a bit to make more organised and add email validation

--- index.js
+++ index.js
@@ -15,13 +15,14 @@ form.addEventListener('submit', (e) => {
         let spanChild = children[2];
         if(inputChild.nodeName !== "SELECT") {
             if(!inputChild.validity.valid) {
-                if(inputChild.attributes.name.nodeValue === "name" && (!inputChild.validity.valueMissing)) {
-                    let min = Number(inputChild.attributes.minLength.nodeValue);
-                    let max = Number(inputChild.attributes.maxLength.nodeValue);
-                    console.log(inputChild.value.length)
-                    errorMsgs.checkNameLength(inputChild, spanChild, min, max)
-                } else {
-                    errorMsgs.requiredErr(inputChild, spanChild)
+                if (inputChild.validity.valueMissing) {
+                    errorMsgs.requiredErr(inputChild, spanChild);
+                } 
+                else if (inputChild.validity.tooShort) {
+                    errorMsgs.checkNameLength(inputChild, spanChild)
+                }
+                else if(inputChild.validity.typeMismatch) {
+                    errorMsgs.checkEmail(inputChild, spanChild)
                 }
             } else {
                 form.reset()
@@ -42,8 +43,11 @@ Array.from(formEls).forEach(item => {
                 errorMsgs.requiredErr(input, error);
             } 
             else if (input.validity.tooShort) {
-                checkNameLength(input, error);
-            } 
+                errorMsgs.checkNameLength(input, error)
+            }
+            else if(input.validity.typeMismatch) {
+                errorMsgs.checkEmail(input, error)
+            }
             else {
                 error.textContent = ""
                 removeErrorMsg(input, error)
@@ -52,14 +56,18 @@ Array.from(formEls).forEach(item => {
 
         console.log(input.validity)
     })
+
     input.addEventListener('blur', (e) => {
         hasBeenBlurred = true;
         if (input.validity.valueMissing) {
             errorMsgs.requiredErr(input, error);
         } 
         else if (input.validity.tooShort) {
-            checkNameLength(input, error);
+            errorMsgs.checkNameLength(input, error)
         } 
+        else if(input.validity.typeMismatch) {
+            errorMsgs.checkEmail(input, error)
+        }
         else {
             error.textContent = ""
             removeErrorMsg(input, error)
@@ -67,27 +75,39 @@ Array.from(formEls).forEach(item => {
     })
 })
 
-const checkIfValid = (input, error) => {
-    if(input.nodeName === "INPUT") {
-        if(!input.validity.valueMissing) {
-            
 
-        } else {
-            errorMsgs.requiredErr(input, error)
+
+const errorMsgs = (() => {
+    const requiredErr = (el, elError) => {
+        elError.textContent = "You need to enter a value";
+        showErrorMsg(el, elError);
+    }
+    
+    const checkNameLength = (el, elError) => {
+        let length = el.value.length
+        if(length < 3) {
+            elError.textContent = `You need to enter at least 3 characters; you have entered only ${length} character/s`;
+        } 
+        else if(length > 20) {
+            elError.textContent = `You have entered to ${length} characters, you may only enter 20`
         }
+        showErrorMsg(el, elError);
     }
-}
 
-const checkNameLength = (input, error) => {
-    if (input.validity.tooShort) {
-        let min = Number(input.attributes.minLength.nodeValue);
-        let max = Number(input.attributes.maxLength.nodeValue);
-        errorMsgs.checkNameLength(input, error, min, max)
-    } else if (input.validity.valid) {
-        error.textContent = ""
-        removeErrorMsg(input, error)
+    const checkEmail = (el, elError) => {
+        console.log(el.validity.typeMismatch);
+        elError.textContent = "Not a valid email";
+        showErrorMsg(el, elError);
     }
-} 
+
+
+    
+    return {
+        requiredErr: requiredErr,
+        checkNameLength: checkNameLength,
+        checkEmail: checkEmail
+    }
+})();
 
 const showErrorMsg = (input, error) => {
     if(!error.classList.contains('show')) {
@@ -107,30 +127,24 @@ const removeErrorMsg = (input, error) => {
     }
 }
 
-const errorMsgs = (() => {
-    const requiredErr = (el, elError) => {
-        if(el.validity.valueMissing) {
-            elError.textContent = "You need to enter a value";
-            showErrorMsg(el, elError);
-        } else  if (el.validity.valid) {
-            removeErrorMsg(el, elError);
-            elError.textContent = "";
-        }
-    }
-
-    const checkNameLength = (el, elError, min, max) => {
-        let length = el.value.length
-        if(length < 3) {
-            elError.textContent = `You need to enter at least ${min} characters; you have entered only ${length} character/s`;
-        } 
-        else if(length > 20) {
-            elError.textContent = `You have entered to ${length} characters, you may only enter ${max}`
-        }
-        showErrorMsg(el, elError);
-    }
-
-    return {
-        requiredErr: requiredErr,
-        checkNameLength: checkNameLength
-    }
-})();
+    // const checkIfValid = (input, error) => {
+    //     if(input.nodeName === "INPUT") {
+    //         if(!input.validity.valueMissing) {
+                
+    
+    //         } else {
+    //             errorMsgs.requiredErr(input, error)
+    //         }
+    //     }
+    // }
+    
+    // const checkNameLength = (input, error) => {
+    //     if (input.validity.tooShort) {
+    //         let min = Number(input.attributes.minLength.nodeValue);
+    //         let max = Number(input.attributes.maxLength.nodeValue);
+    //         errorMsgs.checkNameLength(input, error, min, max)
+    //     } else if (input.validity.valid) {
+    //         error.textContent = ""
+    //         removeErrorMsg(input, error)
+    //     }
+    // } 
